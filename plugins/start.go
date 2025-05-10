c79da8c73f52d0b258c7447a29c8150583650abf
MalluLuffy <123myemailaddressis@gmail.com>
2025-05-10 13:34:19 +0530

    Update start.go

--- plugins/start.go
+++ plugins/start.go
@@ -27,27 +27,29 @@ func Start(bot *gotgbot.Bot, ctx *ext.Context) error {
 
 		return nil
 	}
+	if len(config.FsubChannels) > 0 {
+		var toJoin []*gotgbot.ChatFullInfo
 
-	for _, c := range config.FsubChannels {
-	if !isMember(bot, c, user.Id) {
-		chat, err := bot.GetChat(c, &gotgbot.GetChatOpts{})
-		if err != nil {
-			continue
-		}
+		for _, c := range config.FsubChannels {
+			if !isMember(bot, c, user.Id) {
+				chat, err := bot.GetChat(c, &gotgbot.GetChatOpts{})
+				if err != nil {
+					continue
+				}
 
-		// Ensure invite link is available
-		if chat.InviteLink == "" {
-			invite, err := bot.ExportChatInviteLink(c)
-			if err != nil {
-				fmt.Printf("Failed to export invite link for %d: %v\n", c, err)
-				continue
+				// Ensure invite link is available
+				if chat.InviteLink == "" {
+					invite, err := bot.ExportChatInviteLink(c)
+					if err != nil {
+						fmt.Printf("Failed to export invite link for %d: %v\n", c, err)
+						continue
+					}
+					chat.InviteLink = invite
+				}
+
+				toJoin = append(toJoin, chat)
 			}
-			chat.InviteLink = invite
 		}
-
-		toJoin = append(toJoin, chat)
-	}
-}
 
 		if len(toJoin) > 0 {
 			var buttons [][]gotgbot.InlineKeyboardButton
