4b019f0eb54c1d3ab5f8a955fdf28f6f2f7d2363
MalluLuffy <123myemailaddressis@gmail.com>
2025-05-15 00:57:03 +0530

    Update start.go

--- plugins/start.go
+++ plugins/start.go
@@ -66,7 +66,7 @@ func Start(bot *gotgbot.Bot, ctx *ext.Context) error {
 				}
 			}
 
-			buttons = append(buttons, []gotgbot.InlineKeyboardButton{{Text: "ʀᴇᴛʀʏ 🔃", Url: fmt.Sprintf("https://t.me/%s?start=%s", bot.Username, split[1])}})
+			buttons = append(buttons, []gotgbot.InlineKeyboardButton{{Text: "↻ Tʀʏ Aɢᴀɪɴ", Url: fmt.Sprintf("https://t.me/%s?start=%s", bot.Username, split[1])}})
 
 			update.Reply(bot, format.BasicFormat(config.FsubMessage, user), &gotgbot.SendMessageOpts{ParseMode: gotgbot.ParseModeHTML, ReplyMarkup: gotgbot.InlineKeyboardMarkup{InlineKeyboard: buttons}})
 
