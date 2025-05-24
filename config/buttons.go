5218f282282a38b58b60e87462bda11d67bb799a
MalluLuffy <123myemailaddressis@gmail.com>
2025-05-24 22:35:46 +0530

    Update buttons.go

--- config/buttons.go
+++ config/buttons.go
@@ -7,14 +7,14 @@ package config
 import "github.com/PaulSonOfLars/gotgbot/v2"
 
 var Buttons map[string][][]gotgbot.InlineKeyboardButton = map[string][][]gotgbot.InlineKeyboardButton{
-	"START": {{aboutButton}},
+	"START": {{aboutButton,helpButton}},
 	"ABOUT": {{}},
 	"HELP":  {{}},
 }
 
 // Single buttons used to build composite markups.
 var (
-	aboutButton = gotgbot.InlineKeyboardButton{Text: "ℹ️ About Me", CallbackData: "cmd_ABOUT"}
+	aboutButton = gotgbot.InlineKeyboardButton{Text: "🗿 About Me", CallbackData: "cmd_ABOUT"}
 	helpButton  = gotgbot.InlineKeyboardButton{Text: "🔒 Close", CallbackData: "cmd_HELP"}
 	homeButton  = gotgbot.InlineKeyboardButton{Text: "⬅️ Bᴀᴄᴋ", CallbackData: "cmd_START"}
 )
